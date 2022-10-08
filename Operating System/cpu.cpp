c1bd0b3a8c2932e864d49a52b9c1e5dcf310370e
jangseoyoung98 <77472570+jangseoyoung98@users.noreply.github.com>
2022-10-08 17:06:42 +0900

    completed

--- Operating System/cpu.cpp
+++ Operating System/cpu.cpp
@@ -1,145 +1,44 @@
-#include <Windows.h>	// Sleep() 등 
+// [운영체제] Program Assignment 3_2018380312 장서영
+#include <Windows.h>	
 #include <sysinfoapi.h> // GetSystemInfo()
 #include <winnt.h>		// ULARGE_INTEGER 
-
 #include <stdio.h>
+#include <iostream>
 
-
-// 매크로 상수
 #define MAX_LOOP 100
 #define SLEEP_TIME 1000
 #define MAX_PROCESS 1024
 
-// cpu load 계산 함수 (기준 시간 1 sec)
-double cpuLoad() {
-	
-	/*
-	GetSystemTimes(&IdleT, &KernelT, &UserT);
-	DWORD IdLow1 = IdleT.dwLowDateTime;
-	DWORD IdHigh1 = IdleT.dwHighDateTime;
-	DWORD KeLow1 = KernelT.dwLowDateTime;
-	DWORD KeHigh1 = KernelT.dwHighDateTime;
-	DWORD UsLow1 = UserT.dwLowDateTime;
-	DWORD UsHigh1 = UserT.dwHighDateTime;
+double cpuLoad();
 
-	Sleep(1000);
+int main(int arg, char* argv) { 
 
-	GetSystemTimes(&IdleT, &KernelT, &UserT);
-	DWORD IdLow2 = IdleT.dwLowDateTime;
-	DWORD IdHigh2 = IdleT.dwHighDateTime;
-	DWORD KeLow2 = KernelT.dwLowDateTime;
-	DWORD KeHigh2 = KernelT.dwHighDateTime;
-	DWORD UsLow2 = UserT.dwLowDateTime;
-	DWORD UsHigh2 = UserT.dwHighDateTime;
-	*/
-
-	// DWORD(4 bytes) 대신 ULARE_INTEGER, __int64 사용 -> (https://stackoverflow.com/questions/29266743/what-is-dwlowdatetime-and-dwhighdatetime) 참고
-	
-	FILETIME IdleT, KernelT, UserT;
-	ULARGE_INTEGER IdT, KeT, UsT;
-	BOOL res;
-	
-	res = GetSystemTimes(&IdleT, &KernelT, &UserT);
-	if (res == NULL) 
-		return 0; // if it can't get system times, it will return zero.
-
-	IdT.LowPart = IdleT.dwLowDateTime;
-	IdT.HighPart = IdleT.dwHighDateTime;
-	KeT.LowPart = KernelT.dwLowDateTime;
-	KeT.HighPart = KernelT.dwHighDateTime;
-	UsT.LowPart = UserT.dwLowDateTime;
-	UsT.HighPart = UserT.dwHighDateTime;
-
-	__int64 IdleT_1 = IdT.QuadPart;
-	__int64 KernelT_1 = KeT.QuadPart;
-	__int64 UserT_1 = UsT.QuadPart;
-
-	Sleep(SLEEP_TIME); // 1초 기다림
-
-	GetSystemTimes(&IdleT, &KernelT, &UserT);
-	IdT.LowPart = IdleT.dwLowDateTime;
-	IdT.HighPart = IdleT.dwHighDateTime;
-	KeT.LowPart = KernelT.dwLowDateTime;
-	KeT.HighPart = KernelT.dwHighDateTime;
-	UsT.LowPart = UserT.dwLowDateTime;
-	UsT.HighPart = UserT.dwHighDateTime;
-
-	__int64 IdleT_2 = IdT.QuadPart;
-	__int64 KernelT_2 = KeT.QuadPart;
-	__int64 UserT_2 = UsT.QuadPart;
-
-	// CPU 사용률 -> 1초
-	__int64 IdleTime = IdleT_2 - IdleT_1;
-	__int64 KernelTime = KernelT_2 - KernelT_1;
-	__int64 UserTime = UserT_2 - UserT_1;
-
-	double result = (double)(UserTime + KernelTime - IdleTime) / (UserTime + KernelTime) * 100;
-
-	return result;
-	
-	/* 왜 아래처럼 해야 하는지 이해 안 감..!!
-	// set high, low in FILETIME structure (first time)
-		idle_High = idleTime.dwHighDateTime;
-		idle_Low = idleTime.dwLowDateTime;
-		kernel_High = kernelTime.dwHighDateTime;
-		kernel_Low = kernelTime.dwLowDateTime;
-		user_High = userTime.dwHighDateTime;
-		user_Low = userTime.dwLowDateTime;
-
-		high_1 = idle_High + kernel_High + user_High;
-		Low_1 = idle_Low + kernel_Low + user_Low;
-
-		Sleep(1000); // sleep to get system time for 1 sec
-
-		res = GetSystemTimes(&idleTime, &kernelTime, &userTime); // get system times (idle, kernel, user)
-		if (res == NULL) return 0; // if it can't get system times, it will return zero.
-
-		// set high, low in FILETIME structure (second time)
-		idle_High_2 = idleTime.dwHighDateTime;
-		idle_Low_2 = idleTime.dwLowDateTime;
-		kernel_High_2 = kernelTime.dwHighDateTime;
-		kernel_Low_2 = kernelTime.dwLowDateTime;
-		user_High_2 = userTime.dwHighDateTime;
-		user_Low_2 = userTime.dwLowDateTime;
-
-		high_2 = idle_High_2 + kernel_High_2 + user_High_2;
-		Low_2 = idle_Low_2 + kernel_Low_2 + user_Low_2;
-
-		// calculate time (first time minus second time)
-		idle = idle_Low_2 - idle_Low; // get time in idle mode 
-		kernel = kernel_Low_2 - kernel_Low;  // get time in kernel mode
-		user = user_Low_2 - user_Low;  // get time in user mode
-	*/
-}
-
-
-int main(int arg, char* argv) { // main()의 매개변수 삽입
-
-	// 1. CPU 갯수 -> OK
-	SYSTEM_INFO num;
+	// 1. GetSystemInfo()를 통해 현재 컴퓨터의 CPU 갯수를 출력한다.
+	SYSTEM_INFO num;			
 	GetSystemInfo(&num);
 	printf("Number of CPUs : %d\n", num.dwNumberOfProcessors);
 
-	// 2. cpu load 출력 -> OK..?
-	SYSTEMTIME now;
-	double arr[MAX_PROCESS];
-	double avg5 = 0.0, avg10 = 0.0, avg15 = 0.0;
-	int i = 0;
+	// 2. (코드 하단에 정의한) cpuLoad()를 통해 1초마다 cpu load를 출력하고, 5/10/15초씩 cpu의 평균 load 또한 출력한다.
+	SYSTEMTIME now;									
+	double arr[MAX_PROCESS];						// (함수를 통해 받는) cpu load 값을 보관할 배열을 선언함
+	double avg5 = 0.0, avg10 = 0.0, avg15 = 0.0;	// 5초, 10초, 15초 동안의 cpu 평균 load를 담을 변수를 선언함
+	int i = 0;			
 
-	for (; i < MAX_LOOP; i++) {
+	for (; i < MAX_LOOP; i++) {	// 100번의 cpu laod 출력을 수행함
 
-		arr[i] = cpuLoad();
-
-		GetLocalTime(&now);
+		arr[i] = cpuLoad(); // cpuLoad() 함수를 통해 받은 cpu load 값을 arr[0]~[99]에 순차적으로 저장함
+		
+		GetLocalTime(&now);	// GetLocalTime()을 통해 현재 날짜 및 시간을 출력함
 		printf(" %3d %04d.%02d.%02d %02d:%02d:%02d : ", i, now.wYear, now.wMonth, now.wDay, now.wHour, now.wMinute, now.wSecond);
 
-		if (i == 0) {
+		if (i == 0) {		// i가 0일 때는(첫 번째 줄은) 출력하지 않음 
 			printf("\n");
 			continue;
 		}
 
 		printf("[CPU Load: %3.2f%%] ", arr[i]);
-		
+
+		// i가 5/10/15 이상일 때, (이전) 5초/10초/15초 동안의 cpu load 평균을 계산해 줄력함 
 		if (i >= 5) {
 			avg5 = (arr[i - 1] + arr[i - 2] + arr[i - 3] + arr[i - 4] + arr[i - 5]) / 5;
 			printf("[5sec avg: %3.2f%%] ", avg5);
@@ -157,4 +56,64 @@ int main(int arg, char* argv) { // main()의 매개변수 삽입
 	}
 
 	return 0;
+}
+
+double cpuLoad() {	// cpu load 계산 함수 - 기준 시간 : 1초(1000밀리초)
+
+	// DWORD(4 bytes) 대신 ULARE_INTEGER, __int64 사용 -> (https://stackoverflow.com/questions/29266743/what-is-dwlowdatetime-and-dwhighdatetime) 참고
+	FILETIME IdleT, KernelT, UserT;	// (GetSystemTimes()을 통한 결과값을 담을) FILETIME 구조체 변수 세 개 선언함
+	ULARGE_INTEGER IdT, KeT, UsT;	// (64비트 데이터를 담는) ULARGE_INTEGER 구조체 변수 세 개 선언함
+	BOOL res;						
+
+	// GetSystemtimes()는 idle, kernel, user times를 32비트씩 상단(high)/하단(low) 두 부분으로 나누어 반환한다.
+	// 따라서, 각 times를 LowPart와 HighPart에 나누어서 할당한다.
+	// 이후, ULARGE_INTEGER 구조체의 멤버 QuadPart를 활용해 (64비트의) Idle, Kernel, User 시간을 사용한다.
+	/*
+	  ULARGE_INTEGER members
+	  - LowPart : The low-order 32 bits
+	  - HighPart : The high-order 32 bits
+	  - QuadPart : An unsigned 64-bit integer
+	*/
+
+	// 첫 번째 GetsystemTimes (1초 전)
+	res = GetSystemTimes(&IdleT, &KernelT, &UserT);
+	if (res == NULL) return 0;		// GetSystemtimes에 실패한 경우 zero를 리턴함
+
+	IdT.LowPart = IdleT.dwLowDateTime;
+	IdT.HighPart = IdleT.dwHighDateTime;
+	KeT.LowPart = KernelT.dwLowDateTime;
+	KeT.HighPart = KernelT.dwHighDateTime;
+	UsT.LowPart = UserT.dwLowDateTime;
+	UsT.HighPart = UserT.dwHighDateTime;
+
+	__int64 IdleT_1 = IdT.QuadPart;
+	__int64 KernelT_1 = KeT.QuadPart;
+	__int64 UserT_1 = UsT.QuadPart;
+
+	Sleep(SLEEP_TIME);
+
+	// 두 번째 GetSystemTimes (현재 - 1초 경과 후)
+	res = GetSystemTimes(&IdleT, &KernelT, &UserT);
+	if (res == NULL) return 0;		// GetSystemtimes에 실패한 경우 zero를 리턴함
+
+	IdT.LowPart = IdleT.dwLowDateTime;
+	IdT.HighPart = IdleT.dwHighDateTime;
+	KeT.LowPart = KernelT.dwLowDateTime;
+	KeT.HighPart = KernelT.dwHighDateTime;
+	UsT.LowPart = UserT.dwLowDateTime;
+	UsT.HighPart = UserT.dwHighDateTime;
+
+	__int64 IdleT_2 = IdT.QuadPart;
+	__int64 KernelT_2 = KeT.QuadPart;
+	__int64 UserT_2 = UsT.QuadPart;
+
+	// 1초 동안 CPU가 일 한 시간(cpu load)를 구한다.
+	__int64 IdleTime = IdleT_2 - IdleT_1;
+	__int64 KernelTime = KernelT_2 - KernelT_1;
+	__int64 UserTime = UserT_2 - UserT_1;
+
+	double result = (double)(UserTime + KernelTime - IdleTime) / (UserTime + KernelTime) * 100;
+
+	return result;
+
 }
