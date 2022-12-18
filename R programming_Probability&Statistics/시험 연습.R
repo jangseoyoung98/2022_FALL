d83b37c3aee720de750b92c553ab4dacaec3fa22
jangseoyoung98 <77472570+jangseoyoung98@users.noreply.github.com>
2022-12-18 22:27:45 +0900

    initial commit

---
+++ R programming_Probability&Statistics/시험 연습.R
@@ -0,0 +1,13 @@
+### Initial Settings
+setRepositories(ind = 1:7)
+
+install.packages("data.table")
+library(data.table)
+
+setwd("C:\\Users\\jangs\\Rprojects")
+getwd()
+
+
+
+
+#####################################
